c2036cea9238a0484189969c74f07a69a5f88c9b
bikram2595 <143766781+bikram2595@users.noreply.github.com>
2025-01-24 19:52:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-from setuptools.command.easy_install import auto_chmod
 
 st.set_page_config(layout='wide', page_title = 'StartUp Analysis')
 
